1e6297021c73231fe7d3fba151913a369e24da3e
Eom Jeein <eji@gra-fik.com>
2019-10-23 17:41:07 +0900

    #3.5, #3.6

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 # Register your models here.
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
+
+    """ Custom User Admin """
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "langauge",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
+
+
+# admin.site.register(models.User, CustomUserAdmin) # -> 상단 @admin.register(models.User) 와 같은 뜻임
